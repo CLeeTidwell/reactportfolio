058bb14d3335a9715a39220b2da2149f7da62562
Christopher Tidwell <95063803+CLeeTidwell@users.noreply.github.com>
2022-06-13 15:06:12 -0700

    npm i bootstrap react

---
+++ src/screens/navigationBar/navigationBar.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Navbar from 'react-bootstrap/Navbar'
+import Nav from 'react-bootstrap/Nav'
+
+function NavigationBar() {
+  return (
+    <div>
+      navigation
+    </div>
+  )
+}
+
+export default NavigationBar

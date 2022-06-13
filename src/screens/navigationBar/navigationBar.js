e135145a70128061caa1917b76a6508800b8752e
Christopher Tidwell <95063803+CLeeTidwell@users.noreply.github.com>
2022-06-13 15:08:13 -0700

    import bootstrap CSS

--- src/screens/navigationBar/navigationBar.js
+++ src/screens/navigationBar/navigationBar.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import Navbar from 'react-bootstrap/Navbar'
 import Nav from 'react-bootstrap/Nav'
+import { navigationLinks } from '../../helpers/navigationlinks'
 
 function NavigationBar() {
   return (

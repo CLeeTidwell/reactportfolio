6df4af018c0629df675d2531f695849d1aa6c750
Christopher Tidwell <95063803+CLeeTidwell@users.noreply.github.com>
2022-06-13 16:06:56 -0700

    update js

--- src/screens/navigationBar/navigationBar.js
+++ src/screens/navigationBar/navigationBar.js
@@ -1,12 +1,27 @@
 import React from 'react'
 import Navbar from 'react-bootstrap/Navbar'
 import Nav from 'react-bootstrap/Nav'
-import { navigationLinks } from '../../helpers/navigationlinks'
+import { navigationLinks } from '../../helpers/navigationLinks'
+
+function createLinks() {
+  return navigationLinks.map((e, idx) => (
+    <Nav.Link key={idx} href={e.ref}>{e.name}</Nav.Link>
+  ))
+}
 
 function NavigationBar() {
   return (
-    <div>
-      navigation
+    <div id="home">
+      <Navbar>
+      <Navbar.Brand href="#home">Christopher Tidwell</Navbar.Brand>" 
+      <Navbar.Toggle aria-controls="basic-navbar-nav" /> 
+      <Navbar.Collapse>
+        <Nav>
+      {createLinks()}
+
+        </Nav>
+      </Navbar.Collapse>  
+      </Navbar>  
     </div>
   )
 }

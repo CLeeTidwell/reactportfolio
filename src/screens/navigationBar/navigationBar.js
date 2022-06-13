8b21b846dcdc34a93c66225bebcd20db0b195775
Christopher Tidwell <95063803+CLeeTidwell@users.noreply.github.com>
2022-06-13 16:36:42 -0700

    nav css add

--- src/screens/navigationBar/navigationBar.js
+++ src/screens/navigationBar/navigationBar.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import './navigation.css'
 import Navbar from 'react-bootstrap/Navbar'
 import Nav from 'react-bootstrap/Nav'
 import { navigationLinks } from '../../helpers/navigationLinks'
@@ -12,16 +13,18 @@ function createLinks() {
 function NavigationBar() {
   return (
     <div id="home">
-      <Navbar>
-      <Navbar.Brand href="#home">Christopher Tidwell</Navbar.Brand>" 
-      <Navbar.Toggle aria-controls="basic-navbar-nav" /> 
-      <Navbar.Collapse>
-        <Nav>
-      {createLinks()}
-
-        </Nav>
-      </Navbar.Collapse>  
-      </Navbar>  
+      <Navbar className="navigation__container" style={{
+        zIndex: '2', position: 'fixed', top: '0', width: '100%'
+      }} collapseOnSelect
+        expand="md">
+        <Navbar.Brand style={{ marginLeft: '1rem' }} href="#home">Christopher Tidwell</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse style={{ justifyContent: 'flex-end', marginRight: '1rem', borderColor: 'none' }}>
+          <Nav className="links" style={{ margin: '0 1rem' }}>
+            {createLinks()}
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
     </div>
   )
 }

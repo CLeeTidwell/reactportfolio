737ff0f994bae890b3f402c1e012f462ffc4a44a
Christopher Tidwell <95063803+CLeeTidwell@users.noreply.github.com>
2022-06-15 19:44:52 -0700

    update to expired packages

--- src/screens/header/Header.js
+++ src/screens/header/Header.js
@@ -1,27 +1,54 @@
 import React from 'react'
 import './header.css'
-import Typed from 'react-typed';
+import Typewriter from "typewriter-effect";
 
 function Header() {
   return (
     <div className="main-info">
-      <h1>What am I?</h1>
-      <Typed 
-        strings={[
-          " ", 
-          "A Software Engineer", 
-          "Also known as...", 
-          "Front-end Developer", 
-          "Back-end Developer", 
-          "Full-stack Developer", 
-          "or...", 
-          '"A Coder"', 
-          " ", 
-          " "]}
-          typeSpeed={40}
-          backSpeed={50}
-          loop
-      />
+      <h1>What am I? </h1>
+      
+      <br></br><Typewriter
+  
+  onInit={(typewriter)=> {
+
+  typewriter
+   
+  .typeString("A Software Engineer")
+    
+  .pauseFor(1000)
+  .deleteAll()
+  .typeString("Also known as...",)
+  
+  .pauseFor(1000)
+  .deleteAll()
+  .typeString("A Web Developer")
+
+  .pauseFor(1000)
+  .deleteAll()
+  .typeString("A Web Designer")
+
+  .pauseFor(1000)
+  .deleteAll()
+  .typeString("Front-end Developer")
+
+  .pauseFor(1000)
+  .deleteAll()
+  .typeString("Back-end Developer")
+
+  .pauseFor(1000)
+  .deleteAll()
+  .typeString("Full-stack Developer")
+
+  .pauseFor(1000)
+  .deleteAll()
+  .typeString("or...")
+
+  .pauseFor(1000)
+  .deleteAll()
+  .typeString("'A Coder'")
+  .start();
+  }}
+  />
     </div>
   )
 }
